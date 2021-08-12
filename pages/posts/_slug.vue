e693aba34063bfb13f5a31c3f4d98fe9ccd1c41b
adisak.t <adisak.t@clicknext.com>
2021-08-12 09:43:48 +0700

    add files

---
+++ pages/posts/_slug.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <div>
+        <h1>{{post.title}}</h1>
+        <p>{{post.description}}</p>
+        <hr />
+        <nuxt-content :document="post"></nuxt-content>
+    </div>
+</template>
+<script>
+export default {
+    async asyncData({$content, params}) {
+        const postData = await $content('posts', params.slug).fetch();
+        return{
+            post: postData,
+        };
+    },
+    mounted(){
+        console.log('this.post', this.post);
+    }
+}
+</script>
+<style lang="">
+    
+</style>
